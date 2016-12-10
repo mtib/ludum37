a7a64e1de22232a1dde9d504181b735a2e4471d4
mtib <mtb@tibyte.net>
2016-12-10 22:15:32 +0100

    player facing the way he walkes

--- loader.js
+++ loader.js
@@ -73,22 +73,116 @@ function Player(x, y) {
         return KEY.isDown(KEY[key]);
     }
 
+    getPlayer = function(fname) {
+        let t = getTexture(fname);
+        let scalefactor = 0.8;
+        t.scale.set(scalefactor * GAME.scale.x, scalefactor * GAME.scale.y);
+        t.anchor.set(0.5,1);
+        return t;
+    }
+
+    this.downSprites = [
+        getPlayer("heroF1"), getPlayer("heroF2")
+    ];
+    this.sideSprites = [
+        getPlayer("heroS1"), getPlayer("heroS2")
+    ];
+    this.upSprites = [
+        getPlayer("heroB1"), getPlayer("heroB2")
+    ];
+
+    this.allSprites = [
+        this.upSprites,
+        this.sideSprites,
+        this.downSprites,
+    ];
+
     this.postfix = function() {
-        let scalefactor = 0.8; // TODO WHY ISNT THIS WORKING??
+        let scalefactor = 0.8;
         this.sprite.scale.set(scalefactor * GAME.scale.x, scalefactor * GAME.scale.y);
+        this.sprite.anchor.set(0.5,1);
+    }
+
+    this.show = this.downSprites;
+    this.currentindex = 0;
+    let animmax = 20;
+    this.animstate = animmax;
+    this.currentface = 2;
+
+    this.flip = function(undo = false) {
+        if (undo) {
+            for (var i = 0; i < this.show.length; i++) {
+                this.show[i].scale.x = Math.abs(this.show[i].scale.x);
+            }
+        } else {
+            for (var i = 0; i < this.show.length; i++) {
+                this.show[i].scale.x = -1 * Math.abs(this.show[i].scale.x);
+            }
+        }
+    }
+
+    this.switch_sprite_array = function(to) {
+        GAME.getCurrentStage().removeChild(this.sprite);
+        switch (to) {
+            case 0:
+                this.show = this.upSprites;
+                break;
+            case 1:
+                this.show = this.sideSprites;
+                break;
+            case 2:
+                this.show = this.downSprites;
+                break;
+            case 3:
+                this.show = this.sideSprites;
+                break;
+        }
+        this.sprite = this.show[this.currentindex];
+
+        if (to == 1) {
+            this.flip(false);
+        } else {
+            this.flip(true);
+        }
+        GAME.getCurrentStage().addChild(this.sprite);
+        this.currentface = to;
     }
 
     this.update = function() {
         this.sprite.position.set(this.pos.x, this.pos.y);
+        let n = -1;
         if (isDown("a")) {
             this.pos.x -= deltaT * this.speed * GAME.scale.x;
+            this.animstate -= 1;
+            n = 3;
         } else if (isDown("d")) {
             this.pos.x += deltaT * this.speed * GAME.scale.x;
+            this.animstate -= 1;
+            n = 1;
         }
         if (isDown("w")) {
             this.pos.y -= deltaT * this.speed * this.persfac * GAME.scale.y;
+            this.animstate -= 1;
+            n = 0;
         } else if (isDown("s")) {
             this.pos.y += deltaT * this.speed * this.persfac * GAME.scale.y;
+            this.animstate -= 1;
+            n = 2;
+        }
+        if ( n >= 0 && n != this.currentface ) {
+            this.switch_sprite_array(n);
+        }
+        if ( this.animstate < 0 ) {
+            GAME.getCurrentStage().removeChild(this.sprite);
+            this.animstate = animmax;
+            this.currentindex = (this.currentindex + 1) % this.show.length;
+            this.sprite = this.show[this.currentindex];
+            GAME.getCurrentStage().addChild(this.sprite);
+        }
+        for ( var i = 0; i < this.allSprites.length; i++ ) {
+            for ( var j = 0; j < this.allSprites[i].length; j++) {
+                this.allSprites[i][j].position.set(this.pos.x, this.pos.y);
+            }
         }
     }
 }
@@ -277,19 +371,19 @@ var pointlist = (function() {
         POINTS.fromRel(c1, r2), // 1 c1 middle
         POINTS.fromRel(c1, r3), // 2 c1 bottom
 
-        POINTS.fromRel(.3, r2), // 3 q1
+        POINTS.fromRel(.27, r2), // 3 q1
 
         POINTS.fromRel(c2, r1), // 4
         POINTS.fromRel(c2, r2), // 5
         POINTS.fromRel(c2, r3), // 6
 
-        POINTS.fromRel(c2+.2, r2), // 7 q2
+        POINTS.fromRel(c2+.1, r2), // 7 q2
 
         POINTS.fromRel(c3, r1), // 8
         POINTS.fromRel(c3, r2), // 9
         POINTS.fromRel(c3, r3), // 10
 
-        POINTS.fromRel(c3-.2, r2), // 11 q3
+        POINTS.fromRel(c3-.12, r2), // 11 q3
 
         POINTS.fromRel(.666,.228) // 12 ticket maschine
     ];
