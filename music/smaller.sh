2f2e0ca8855a6e803efbbc1b7a93f08d17ade441
mtib <mtb@tibyte.net>
2016-12-10 15:27:31 +0100

    music and images

--- music/smaller.sh
+++ music/smaller.sh
@@ -2,8 +2,8 @@
 name=$1
 
 mkdir .tmp
-for ext in ".ogg" ".mp3"; do
+for ext in "ogg" "mp3"; do
     ffmpeg -i "$name.$ext" -ar 44100 -ac 1 -ab 96k "o_$name.$ext"
-    mv "$name.$ext" ".tmp/$name_old.$ext"
+    mv "$name.$ext" ".tmp/${name}_old.$ext"
     mv "o_$name.$ext" "$name.$ext"
 done
